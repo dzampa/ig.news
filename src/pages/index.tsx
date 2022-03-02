94f4a5a0d6f8704bf7e6b2e80c449f3ae956fcd0
Diego Zamparo <diego_zamparo@hotmail.com>
2022-03-02 09:48:51 -0300

    SSG applied at home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeBottom } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -36,7 +36,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KYqzqGFpg8DqaAwUb0H22Qg');
 
   const product = {
@@ -50,6 +50,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
