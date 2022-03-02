9a868991176b90f2c2826f82e1e22b48eadba1eb
Diego Zamparo <diego_zamparo@hotmail.com>
2022-03-02 09:51:09 -0300

    removed static code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home({ product }: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve('price_1KYqzqGFpg8DqaAwUb0H22Qg');
+  const price = await stripe.prices.retrieve(process.env.SUBSCRIBE_KEY);
 
   const product = {
     priceId: price.id,
