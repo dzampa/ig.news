39a606250524e070fb9003d0a9abc44dbffd4c3f
Diego Zamparo <diego_zamparo@hotmail.com>
2022-03-03 21:09:43 -0300

    added git authenticate

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,26 @@
 import {FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
 import styles from './styles.module.scss';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession()
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04d361"/>
-            Diego Zamparo
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ) : (        
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={()=> signIn('github')}
         >
             <FaGithub color="#eba417"/>
             Sign in with GitHub
