a29f57218dcdb375d3aaf78cfda72819547158bd
Diego Zamparo <diego_zamparo@hotmail.com>
2022-03-05 18:48:06 -0300

    add payment with stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeBottomProps {
@@ -5,10 +8,32 @@ interface SubscribeBottomProps {
 }
 
 export function SubscribeBottom({ priceId }: SubscribeBottomProps) {
+    const session = useSession();
+
+    async function handleSubscribe(){
+        if (!session.data) {
+            signIn('github')
+            return;
+        }
+
+        try { 
+            const response = await api.post('/subscribe')
+
+            const {sessionId} = response.data;
+
+            const stripe = await getStripeJs();
+
+            await stripe.redirectToCheckout({sessionId})
+        } catch (err) {
+            alert(err.message);
+        }
+    }
+
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now       
         </button>
