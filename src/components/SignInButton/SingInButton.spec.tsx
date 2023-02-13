504b191a80df65179349c1f171719b5a25d26838
Diego Zamparo <diego_zamparo@hotmail.com>
2023-02-13 08:03:10 -0300

    SingInButton test added

---
+++ src/components/SignInButton/SingInButton.spec.tsx
@@ -0,0 +1,27 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react'
+import { mocked } from 'jest-mock'
+import { useSession } from 'next-auth/react';
+import { SignInButton } from '.'
+
+jest.mock('next-auth/react')
+
+describe('SignInButton component', () => {
+    it('renders correctly when user is not authenticated', () => {
+        const useSessionMocked = mocked(useSession)
+
+        useSessionMocked.mockReturnValueOnce({ data: null, status: 'unauthenticated' })
+
+        render(<SignInButton />)
+        expect(screen.getByText('Sign in with GitHub')).toBeInTheDocument()
+    })
+
+    it('renders correctly when user is authenticated', () => {
+        const useSessionMocked = mocked(useSession)
+
+        useSessionMocked.mockReturnValueOnce({ data: { user: { name: 'John Doe', email: 'john.doe@gmail.com' }, expires: 'fake-expires' }, status: 'authenticated' })
+
+        render(<SignInButton />)
+        expect(screen.getByText('John Doe')).toBeInTheDocument()
+    })
+})
