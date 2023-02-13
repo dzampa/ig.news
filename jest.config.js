82f95a631acdc97703b7d242a115eae2aa5190a1
Diego Zamparo <diego_zamparo@hotmail.com>
2023-02-13 07:33:31 -0300

    added test config and testes with jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    testPathIgnorePattern: ["/node_module/", "/.next"],
+    setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+    },
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        "\\.(scss|css|sass)$": "identity-obj-proxy"
+    }
+};
